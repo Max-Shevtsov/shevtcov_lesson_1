8d5c9183e8fbfb97ad55684f2da5ca832edfcf4d
Max <maxschew@yandex.ru>
2023-02-08 23:29:09 +0200

    up compileSdk, bind data and adapter

--- app/src/main/java/com/example/shevtcov_lesson_1/MainActivity.kt
+++ app/src/main/java/com/example/shevtcov_lesson_1/MainActivity.kt
@@ -2,12 +2,40 @@ package com.example.shevtcov_lesson_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.shevtcov_lesson_1.adapters.StudentsRecyclerAdapter
+import com.example.shevtcov_lesson_1.data.StudentsRepository
+import com.example.shevtcov_lesson_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val repository = StudentsRepository
+
+    private val adapter: StudentsRecyclerAdapter = StudentsRecyclerAdapter()
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initAdapter()
+        renderState()
     }
 
+    private fun initAdapter() {
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.apply {
+            rvStudent.layoutManager = layoutManager
+            rvStudent.adapter = adapter
+        }
+    }
+
+    private fun renderState() = binding.apply {
+            adapter.submitList(repository.getStudents())
+        }
 
 }
--- app/src/main/java/com/example/shevtcov_lesson_1/data/StudentsRepository.kt
+++ app/src/main/java/com/example/shevtcov_lesson_1/data/StudentsRepository.kt
@@ -4,7 +4,16 @@ import com.example.shevtcov_lesson_1.models.Student
 
 object StudentsRepository {
 
-    fun createStudent(): List<Student> {
+    private var students: List<Student> = emptyList()
+
+    fun getStudents(): List<Student> {
+        return students.ifEmpty {
+            students = createStudent()
+            students
+        }
+    }
+
+    private fun createStudent(): List<Student> {
         return (0..30).toList().map { index ->
             Student(
                 age = index,
