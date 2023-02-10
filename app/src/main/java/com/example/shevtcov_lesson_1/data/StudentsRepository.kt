54a1c10f03365e874f39911d34848ca9c6703ea6
Maxim Shevtcov <113129988+Max-Shevtsov@users.noreply.github.com>
2023-02-10 17:15:55 +0200

    Revert "first pull request"

--- app/src/main/java/com/example/shevtcov_lesson_1/data/StudentsRepository.kt
+++ app/src/main/java/com/example/shevtcov_lesson_1/data/StudentsRepository.kt
@@ -4,29 +4,23 @@ import com.example.shevtcov_lesson_1.models.Student
 
 object StudentsRepository {
 
-    fun getStudent(studentType: StudentType): List<Student> {
-        val countStudents = when (studentType) {
-            is StudentType.MGU -> 50
-            is StudentType.KGTU -> 35
-            is StudentType.BFU -> 20
+    private var students: List<Student> = emptyList()
+
+    fun getStudents(): List<Student> {
+        return students.ifEmpty {
+            students = createStudent()
+            students
         }
-        return (0..countStudents).toList().map { index ->
+    }
+
+    private fun createStudent(): List<Student> {
+        return (0..30).toList().map { index ->
             Student(
                 age = index,
                 name = "student$index",
                 email = "student$index@mail.ru",
             )
         }
-    }
-
-    sealed class StudentType {
-
-        object MGU: StudentType()
-
-        object KGTU: StudentType()
-
-        object BFU: StudentType()
-
     }
 
 }
